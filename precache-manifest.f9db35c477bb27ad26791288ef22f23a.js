9859fde16408ef1bb7819c153f74456301c174b0
Dedal378 <orlovetsfoto@gmail.com>
2020-12-04 16:14:10 +0300

    Initial commit

---
+++ precache-manifest.f9db35c477bb27ad26791288ef22f23a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127f52d169b0fc268476",
+    "url": "css/app.386051e8.css"
+  },
+  {
+    "revision": "81eb1ba333f2fb4635b2",
+    "url": "css/chunk-vendors.681f5333.css"
+  },
+  {
+    "revision": "f169c24e3f4213a878ec06eb97eb9e31",
+    "url": "index.html"
+  },
+  {
+    "revision": "127f52d169b0fc268476",
+    "url": "js/app.42ba4747.js"
+  },
+  {
+    "revision": "81eb1ba333f2fb4635b2",
+    "url": "js/chunk-vendors.99f954e3.js"
+  },
+  {
+    "revision": "4e69469f170c01911bda9273d155f21e",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
